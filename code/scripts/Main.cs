8179a998f4b7dfd636478aeda920bad1bc9af7cf
Fred <64082311+MatiP911@users.noreply.github.com>
2025-07-31 14:40:51 +0200

    jakies naprawy, ale nadal bajzel

--- code/scripts/Main.cs
+++ code/scripts/Main.cs
@@ -3,4 +3,33 @@ using System;
 
 public partial class Main : Node2D
 {
+    private Node sceneContainer;
+
+    public override void _Ready()
+    {
+        sceneContainer = GetNode("SceneContainer");
+
+        LoadScene("res://level/scene/all.tscn");
+    }
+
+    public void LoadScene(string scenePath)
+    {
+        // Clear last scene
+        foreach (Node child in sceneContainer.GetChildren())
+        {
+            child.QueueFree();
+        }
+
+        // Load new scene
+        var packed = GD.Load<PackedScene>(scenePath);
+        if (packed != null)
+        {
+            var instance = packed.Instantiate();
+            sceneContainer.AddChild(instance);
+        }
+        else
+        {
+            GD.PushError("Can not load scene: " + scenePath);
+        }
+    }
 }
---
+++ code/scripts/all.cs
@@ -0,0 +1,6 @@
+using Godot;
+using System;
+
+public partial class All : Node2D
+{
+}
