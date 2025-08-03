a4fadbb332b64d21eec9e5767ee002ba27aa4fb1
Michał Kozłowski <micho.kozlowski@gmail.com>
2025-08-03 16:42:13 +0200

    początek cutscenki

---
+++ code/scripts/Global.gd
@@ -0,0 +1,4 @@
+extends Node
+
+# Will be false until the very first time we play the cutscene
+var skipCutscene: bool = false
--- code/scripts/all.gd
+++ code/scripts/all.gd
@@ -1,11 +1,29 @@
 extends Node2D
 
 func _ready():
+	if Global.skipCutscene == true:
+		$Bomb1.visible = true
+		$Bomb2.visible = true
+	else:
+		$Notatka.visible = true
+		Global.skipCutscene = true
 	$Bomb1.connect("input_event", Callable(self, "_on_Bomb1Collision_input_event"))
 	$Bomb2.connect("input_event", Callable(self, "_on_Bomb2Collision_input_event"))
+	$Notatka.connect("input_event", Callable(self, "_on_NotatkaCollision_input_event"))
+	$List.connect("input_event", Callable(self, "_on_ListCollision_input_event"))
+	
 
 
+func _on_NotatkaCollision_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		$Notatka.visible = false
+		$List.visible = true
 
+func _on_ListCollision_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		$List.visible = false
+		$Bomb1.visible = true
+		$Bomb2.visible = true
 
 func _on_Bomb1Collision_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
